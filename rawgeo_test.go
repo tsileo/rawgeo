3fab104876bda4f28d31c580b5ad92fcece4f82f
Thomas Sileo <thomas.sileo@gmail.com>
2016-04-01 22:52:45 +0200

    added basic unit tests

---
+++ rawgeo_test.go
@@ -0,0 +1,76 @@
+package rawgeo
+
+import (
+	log "gopkg.in/inconshreveable/log15.v2"
+
+	"testing"
+)
+
+var Paris = &Point{Lat: 48.864716, Lng: 2.349014, ID: "paris"}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestRawGeoQueryInsertedPoint(t *testing.T) {
+	Log.SetHandler(log.LvlFilterHandler(log.LvlDebug, log.StdoutHandler))
+	tdb, err := New("test_1.db")
+	check(err)
+	defer tdb.Destroy()
+
+	check(tdb.Index(Paris))
+
+	// Check with a 10meters radius that we can retrieve the previously inserted point
+	res, err := tdb.Query(Paris.Lat, Paris.Lng, 1)
+	check(err)
+
+	if len(res) != 1 {
+		t.Errorf("Query should return 1 point, got %d", len(res))
+	}
+
+	if len(res) == 0 {
+		t.Error("no results found, expected 1")
+	} else {
+		if int(res[0].Distance) != 0 {
+			t.Errorf("Expected distance 0 from query, got %f", res[0].Distance)
+		}
+	}
+}
+
+func TestRawGeo(t *testing.T) {
+	Log.SetHandler(log.LvlFilterHandler(log.LvlDebug, log.StdoutHandler))
+	log.Root().SetHandler(log.StdoutHandler)
+	tdb, err := New("test_2.db")
+	check(err)
+	defer tdb.Destroy()
+
+	farFromParis := &Point{
+		ID:  "500km",
+		Lat: 44.36810797040634,
+		Lng: 2.349014000000026,
+	}
+	nearParis := &Point{
+		ID:  "50km",
+		Lat: 48.41505519704064,
+		Lng: 2.349014000000026,
+	}
+
+	check(tdb.Index(farFromParis))
+	check(tdb.Index(nearParis))
+
+	res, err := tdb.Query(Paris.Lat, Paris.Lng, 60000)
+	check(err)
+
+	if len(res) != 1 {
+		t.Errorf("expected 1 results, got %d", len(res))
+	}
+
+	res, err = tdb.Query(Paris.Lat, Paris.Lng, 600000)
+	check(err)
+
+	if len(res) != 2 {
+		t.Errorf("expected 2 results, got %d", len(res))
+	}
+}
