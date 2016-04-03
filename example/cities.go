272678857db1da25b095fd53db5633c35a4912bf
Thomas Sileo <thomas.sileo@gmail.com>
2016-04-03 15:05:16 +0200

    simplified example

--- example/cities.go
+++ example/cities.go
@@ -11,25 +11,6 @@ import (
 	"github.com/tsileo/rawgeo"
 )
 
-type City struct {
-	GeonameID      int
-	Name           string   //  name of geographical point (utf8) varchar(200)
-	AsciiName      string   //  name of geographical point in plain ascii characters, varchar(200)
-	AlternateNames []string //  alternatenames, comma separated, ascii names automatically transliterated, convenience attribute from alternatename table, varchar(10000)
-	Latitude       float64  //  latitude in decimal degrees (wgs84)
-	Longitude      float64  //  longitude in decimal degrees (wgs84)
-	FeatureClass   string   //  see httpstring // //www.geonames.org/export/codes.html, char(1)
-	FeatureCode    string   //  see httpstring // //www.geonames.org/export/codes.html, varchar(10)
-	CountryCode    string   //  ISO-3166 2-letter country code, 2 characters
-	CC2            string   //  alternate country codes, comma separated, ISO-3166 2-letter country code, 60 characters
-	/* Skip 4 Admin */
-	Population int       //  bigint (8 byte int)
-	Elevation  int       //  in meters, integer
-	Dem        string    //  digital elevation model, srtm3 or gtopo30, average elevation of 3''x3'' (ca 90mx90m) or 30''x30'' (ca 900mx900m) area in meters, integer. srtm processed by cgiar/ciat.
-	Timezone   string    //  the timezone id (see file timeZone.txt) varchar(40)
-	Modified   time.Time //  date of last modification in yyyy-MM-dd format
-}
-
 func parseLocation(db *rawgeo.RawGeo) error {
 	file, err := os.Open("cities1000.txt")
 	if err != nil {
@@ -37,9 +18,6 @@ func parseLocation(db *rawgeo.RawGeo) error {
 	}
 	defer file.Close()
 	scan := bufio.NewScanner(file)
-	t := time.Now()
-
-	n := 0
 	for scan.Scan() {
 		line := scan.Text()
 		s := strings.Split(line, "\t")
@@ -58,11 +36,6 @@ func parseLocation(db *rawgeo.RawGeo) error {
 			ID:  fmt.Sprintf("%s-%s", s[8], s[1]),
 			Lat: lat,
 			Lng: lon,
-			// Data: map[string]interface{}{
-			// 	"city":    row[1],
-			// 	"country": row[0],
-			// 	"region":  row[3],
-			// },
 		}
 		if err := db.Index(p); err != nil {
 			if err == rawgeo.ErrInvalidLatLong {
@@ -70,15 +43,8 @@ func parseLocation(db *rawgeo.RawGeo) error {
 			}
 			return err
 		}
-		n++
 	}
-	d := time.Since(t)
-	fmt.Printf("Processed %d entries in %v, %.1f entries/sec.", n, d, float64(n)/(float64(d)/float64(time.Second)))
 	return nil
-}
-
-func search(lat, lon, radius float64, db *rawgeo.RawGeo) ([]*rawgeo.Point, error) {
-	return db.Query(lat, lon, radius)
 }
 
 func main() {
@@ -91,9 +57,10 @@ func main() {
 		panic(err)
 	}
 	n := time.Now()
-	res, err := search(30.26715, -97.74306, 40, db)
+	// Query should return Austin TX
+	res, err := db.Query(30.26715, -97.74306, 40) // 40m
 	took := time.Since(n)
-	fmt.Printf("took %v\n", took)
+	fmt.Printf("query took %v\n", took)
 	if res != nil && len(res) > 0 {
 		fmt.Printf("res=%+v", res[0])
 	} else {
